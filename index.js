6224e1543534de03be30b7f3d4fd09531a229133
Oko Christian <okochristian6@gmail.com>
2026-04-01 14:27:12 +0100

    fixed deployment error

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv"
-import connectDB from "./config/database.js";
-import app from "./app.js";
+import connectDB from "./src/config/database.js";
+import app from "./src/app.js";
 
 dotenv.config({
     path: "./.env"
